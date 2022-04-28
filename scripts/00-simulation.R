cea55e346638196fd8ad10aba4e974311e7e3f35
anindya <aauveek@kinaxis.com>
2022-04-27 21:20:38 -0400

    remove large data, simulation and final paper

--- scripts/00-simulation.R
+++ scripts/00-simulation.R
@@ -3,116 +3,165 @@
 
 #assign column names
 
-cols <- c("Urban", "Rural", "Mountain" , "Hill","Terai",  "Eastern" , "Central", "Western" ,"Mid-western" ,"Far.western", "Primary", "Secondary+","No Education", 
-          "No antenatal or delivery care" , "Either antenatal or delivery care","Both antenatal and delivery care" )
+cols <- c("othshelp", "givblood", "givhmlss", "cutahead", "volchrty", "loanitem",
+          "careself", "peoptrbl", "selffrst", "helpfrds", 
+          "helppoor", "helpnot", "helpsick", "helpblk", "trdunion", 
+          "goveqinc", "govjobs", "govunemp", "govlazy", "govcare",
+          "partyid","polviews")
 
 #create empty dataframe with 1000 rows
 
 n <- 1000
-df <- data.frame(matrix(ncol = 0, nrow = n))
+gss_data <- data.frame(matrix(ncol = 0, nrow = n))
 
 # add simulated values to columns
 for (i in cols){
-  m <- runif(1, min=0, max=1)
-  df[i] <- abs(rnorm(n, mean = m, sd = 0.05))
+  m <- runif(1, min=1, max=7)
+  d <- runif(1, 1, 3)
+  gss_data[i] <- abs(as.integer(rnorm(n, mean = m, sd = d)))
 }
+m <- runif(1, min=0, max=1)
+gss_data["binary_orientation"] <- abs(as.integer(rnorm(n, mean = m, sd = 0.3)))
 
 # Calculate Means of the variables
 
-mean_selected <- df
-MeanMortality <- vector(mode = "character", length = ncol(mean_selected))
-SocioEconomicGroup <- vector(mode = "character", length = ncol(mean_selected))
+
+Attribute_Name <- vector(mode = "character", length = ncol(gss_data))
+Mean <- vector(mode = "character", length = ncol(gss_data))
+Standard_Deviation <- vector(mode = "character", length = ncol(gss_data))
+
 count <- 1
-for (i in colnames(mean_selected)){
-  MeanMortality[count] <- signif(mean(as.numeric(unlist(na.omit(mean_selected[i])))), 3)
-  SocioEconomicGroup[count] <- i
+
+for (i in colnames(gss_data)){
+  Mean[count] <- signif(mean(as.numeric(unlist(na.omit(gss_data[i])))), 3)
+  Standard_Deviation[count] <- signif(sd(as.numeric(unlist(na.omit(gss_data[i])))),3)
+  Attribute_Name[count] <- i
   count <- count + 1
 }
-mean_table <- data.frame(SocioEconomicGroup, MeanMortality)
 
-knitr::kable(mean_table, caption = "Mean Mortality Rates of Selected Socio-economic Groupings") %>%
+
+mean_sd_table <- data.frame(Attribute_Name, Mean, Standard_Deviation)
+
+knitr::kable(mean_sd_table, caption = "Table I: Means and standard Deviations of Selected Variables") %>%
   kable_styling(latex_options="scale_down")
 
-# Plot Bar Charts of the Variables
-
-selected_df <- select(df, Urban, Rural, InfantAgeGroup)
-df3 <- melt(selected_df, id.vars='InfantAgeGroup')
-ggplot(df3, aes(x=InfantAgeGroup, y=value, fill=variable)) +
-  geom_bar(stat='identity', position='dodge') + scale_x_discrete(guide = guide_axis(n.dodge=3)) + ggtitle("Plot 1: Mortality Rates of children by Residence Type") + guides(fill=guide_legend(title="Residence")) + ylab("Mortality Rate (Death per 1000)") 
-
-selected_df <- select(df, "Primary", "Secondary+", "No Education", "InfantAgeGroup")
-df3 <- melt(selected_df, id.vars='InfantAgeGroup')
-ggplot(df3, aes(x=InfantAgeGroup, y=value, fill=variable)) +
-  geom_bar(stat='identity', position='dodge') + scale_x_discrete(guide = guide_axis(n.dodge=3)) + guides(fill=guide_legend(title="Education")) + ylab("Mortality Rate (Death per 1000)") + ggtitle("Plot 2: Mortality Rates of children by Education Level")
-
-selected_df <- select(df,  "Mountain" ,"Hill", "Terai", "InfantAgeGroup")
-df3 <- melt(selected_df, id.vars='InfantAgeGroup')
-ggplot(df3, aes(x=InfantAgeGroup, y=value, fill=variable)) +
-  geom_bar(stat='identity', position='dodge') + scale_x_discrete(guide = guide_axis(n.dodge=3)) + guides(fill=guide_legend(title="Ecological Region")) + ylab("Mortality Rate (Death per 1000)") + ggtitle("Plot 3: Mortality Rates of children by Ecologicial Region")
-
-selected_df <- select(df, "Eastern","Central","Western","Mid-western","Far.western","InfantAgeGroup")
-df3 <- melt(selected_df, id.vars='InfantAgeGroup')
-ggplot(df3, aes(x=InfantAgeGroup, y=value, fill=variable)) +
-  geom_bar(stat='identity', position='dodge') + scale_x_discrete(guide = guide_axis(n.dodge=3)) + guides(fill=guide_legend(title="Geographical Region")) + ylab("Mortality Rate (Death per 1000)") + ggtitle("Plot 4: Mortality Rates of children by Geographical Region")
 
 
-# Conduct T test with the Variables
+# Distribution of Variables
 
-ttest_df <- data.frame(matrix(ncol = 4, nrow = 0))
-colnames(ttest_df) <- c("X1", "X2", "Type", "P-Value")
-# T tests 
+counts_table <- data.frame(Attribute = character(ncol(gss_data)), b = character(ncol(gss_data)), c = character(ncol(gss_data)),
+                           d = character(ncol(gss_data)), e = character(ncol(gss_data)), f = character(ncol(gss_data)),
+                           g = character(ncol(gss_data)), h = character(ncol(gss_data)), i = character(ncol(gss_data)))
 
-ttest1 <- t.test(as.numeric(df$Urban), as.numeric(df$Rural), alternative='less')
-ttest_df[1, ] <- c("Urban", "Rural", "Less", signif(ttest1$p.value, 3))
 
-ttest2 <- t.test(as.numeric(df$Mountain), as.numeric(df$Hill), alternative='greater')
-ttest_df[2, ] <- c("Mountain", "Hill", "Greater", signif(ttest2$p.value, 3))
+for (i in seq(1,ncol(gss_data))){
+  atr <- colnames(gss_data)[i]
+  if (atr != "coninc"){
+    cts <- table(gss_data[,i])
+    for (j in seq(1,length(cts))){
+      counts_table[i, j+1] <- signif(as.numeric(cts[j])/sum(cts), 5)
+    }
+    counts_table[i, 1] <- atr
+  }
+}
 
-ttest3 <- t.test(as.numeric(df$Mountain), as.numeric(df$Terai), alternative='greater')
-ttest_df[3, ] <- c("Mountain", "Terai", "Greater", signif(ttest3$p.value, 3))
+colnames(counts_table) <- c("AttributeName", "1", "2", "3", "4", "5", "6", "7", "8")
 
-ttest4 <- t.test(as.numeric(df$Hill), as.numeric(df$Terai), alternative='less')
-ttest_df[4, ] <- c("Hill", "Terai", "Less", signif(ttest4$p.value, 3))
-
-ttest5 <- t.test(as.numeric(df$Primary), as.numeric(df$`No Education`), alternative='less')
-ttest_df[5, ] <- c("Primary", "No Education", "Less", signif(ttest5$p.value, 3))
-
-ttest6 <- t.test(as.numeric(df$`Secondary+`), as.numeric(df$`No Education`), alternative='less')
-ttest_df[6, ] <- c("Secondary +", "No Education", "Less", signif(ttest6$p.value, 3))
-
-ttest7 <- t.test(as.numeric(df$`Secondary+`), as.numeric(df$Primary), alternative='less')
-ttest_df[7, ] <- c("Secondary +", "Primary", "Less", signif(ttest7$p.value, 3))
-
-ttest8 <- t.test(as.numeric(df$Eastern), as.numeric(df$Western), alternative='less')
-ttest_df[8, ] <- c("Eastern", "Western", "Less", signif(ttest8$p.value, 3))
-
-ttest9 <- t.test(as.numeric(df$Eastern), as.numeric(df$Central), alternative='less')
-ttest_df[9, ] <- c("Eastern", "Central", "Less", signif(ttest9$p.value, 3))
-
-ttest10 <- t.test(as.numeric(df$Eastern), as.numeric(df$`Mid-western`), alternative='less')
-ttest_df[10, ] <- c("Eastern", "Mid Western", "Less", signif(ttest10$p.value, 3))
-
-ttest11 <- t.test(as.numeric(df$Eastern), as.numeric(df$Far.western), alternative='less')
-ttest_df[11, ] <- c("Eastern", "Far Western", "Less", signif(ttest11$p.value, 3))
-
-ttest12 <- t.test(as.numeric(df$Western), as.numeric(df$Central), alternative='less')
-ttest_df[12, ] <- c("Western", "Central", "Less", signif(ttest12$p.value, 3))
-
-ttest13 <- t.test(as.numeric(df$Western), as.numeric(df$`Mid-western`), alternative='less')
-ttest_df[13, ] <- c("Western", "Mid Western", "Less", signif(ttest13$p.value, 3))
-
-ttest14 <- t.test(as.numeric(df$Western), as.numeric(df$Far.western), alternative='less')
-ttest_df[14, ] <- c("Western", "Far Western", "Less", signif(ttest14$p.value, 3))
-
-ttest15 <- t.test(as.numeric(df$Central), as.numeric(df$`Mid-western`), alternative='less')
-ttest_df[15, ] <- c("Central", "Mid Western", "Less", signif(ttest15$p.value, 3))
-
-ttest16 <- t.test(as.numeric(df$Central), as.numeric(df$Far.western), alternative='less')
-ttest_df[16, ] <- c("Central", "Far Western", "Less", signif(ttest16$p.value, 3))
-
-ttest17 <- t.test(as.numeric(df$`Mid-western`), as.numeric(df$Far.western), alternative='less')
-ttest_df[17, ] <- c("Mid Western", "Far Western", "Less", signif(ttest17$p.value, 3))
-
-knitr::kable(ttest_df, caption = "T-Test of Mortality Rates by Socioeconomic Factors") %>%
+knitr::kable(counts_table, caption = "Table II: Categorical Variable Frequencies") %>%
   kable_styling(latex_options="scale_down")
+
+# Conduct Chisq test with the Altruistic Variables and Polviews
+
+Altruistic_cols <- c("othshelp", "givblood", "givhmlss", "cutahead", "volchrty", "loanitem", "careself", "peoptrbl", "selffrst", "helpfrds")
+Chisq_table <- data.frame(matrix(ncol = length(Altruistic_cols) + 1, nrow = 1))
+colnames(Chisq_table) <- c("", Altruistic_cols)
+
+Chisq_table[1,1] <- "P-Val"
+for (j in seq(1, length(Altruistic_cols))){
+  temp_df <- na.omit(dplyr::select(gss_data, "polviews", Altruistic_cols[j]))
+  if (nrow(temp_df) > 500){
+    pval <- chisq.test(temp_df[,1], temp_df[,2])$p.value
+    if (pval < 0.05){
+      Chisq_table[1, j+1] <- signif(pval, 2)
+    } else {
+      Chisq_table[1, j+1] <- "> 0.05"
+    }
+  }
+}
+
+knitr::kable(Chisq_table, caption = "Table IV: P Value of Chi Square Test Comparing Altruistic Variables and Political Orientation") %>%
+  kable_styling(latex_options="scale_down")
+
+
+# Conduct Chisq test with the Political Opinion Variables and Polviews
+
+Pol_cols <- c("helppoor", "helpnot", "helpsick", "helpblk", "trdunion", "goveqinc", "govjobs", "govunemp", "govlazy", "govcare")
+Chisq_table <- data.frame(matrix(ncol = length(Pol_cols) + 1, nrow = 1))
+colnames(Chisq_table) <- c("", Pol_cols)
+
+Chisq_table[1,1] <- "P-Val"
+for (j in seq(1, length(Pol_cols))){
+  temp_df <- na.omit(dplyr::select(gss_data, 'polviews', Pol_cols[j]))
+  if (nrow(temp_df) > 500){
+    pval <- chisq.test(temp_df[,1], temp_df[,2])$p.value
+    if (pval < 0.05){
+      Chisq_table[1, j+1] <- signif(pval, 2)
+    } else {
+      Chisq_table[1, j+1] <- "> 0.05"
+    }
+  } else{
+    Chisq_table[1, j+1] <- NA
+  }
+  
+}
+
+
+knitr::kable(Chisq_table, caption = "Table IV: P Value of Chi Square Test Comparing Political Stance Variables and Political Orientation") %>%
+  kable_styling(latex_options="scale_down")
+
+
+# Logistic Regression Model Altruistism Variables and Polviews
+
+Log_reg_table <- data.frame(matrix(ncol = length(Altruistic_cols) + 1, 1))
+colnames(Log_reg_table) <- c("",Altruistic_cols)
+
+Log_reg_table[1,1] <- "Coefficient"
+for (j in seq(1, length(Altruistic_cols))){
+  temp_df <- na.omit(dplyr::select(gss_data, "polviews", Altruistic_cols[j]))
+  colnames(temp_df) <- c("a", "b")
+  if (nrow(temp_df) > 500){
+    pval <- chisq.test(temp_df[,1], temp_df[,2])$p.value
+    if (pval < 0.05){
+      temp_model <- polr(as.factor(a) ~ b, data = temp_df)
+      Log_reg_table[1, j+1] <- signif(as.numeric(temp_model$coefficients), 3)
+    } else {
+      Log_reg_table[1, j+1] <- "P > 0.05"
+    }
+  }
+}
+
+knitr::kable(Log_reg_table, caption = "Table V: Coefficient of Logistic Regression of Altruistic Variables and Polviews") %>%
+  kable_styling(latex_options="scale_down")
+
+# Logistic Regression Model Political Opinion Variables and Binary Orientation
+
+Log_reg_table <- data.frame(matrix(ncol = 7, nrow = 1))
+
+Pol_cols <- c("helppoor", "helpnot", "helpsick", "helpblk", "trdunion", "goveqinc")
+
+pol_df <- na.omit(dplyr::select(gss_data, "binary_orientation", Pol_cols))
+mdl <- glm(binary_orientation ~ helppoor + helpnot + helpsick + helpblk + trdunion + goveqinc, family = binomial(link = "logit"), data = pol_df)
+
+colnames(Log_reg_table) <- c("", Pol_cols)
+
+Log_reg_table[1, 1] <- "P-Val"
+
+for (i in seq(1, length(Pol_cols))){
+  Log_reg_table[1, i+1] <- as.numeric(coef(summary(mdl))[,4][Pol_cols[i]])
+}
+
+knitr::kable(Log_reg_table, caption = "Table IV: P Value of Logistic Regression Model of Political Stance Variables and Voted Candidate") %>%
+  kable_styling(latex_options="scale_down")
+
+
 
